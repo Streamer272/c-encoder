ed835ef905f87372f3bf6ffc133aa4fa8999ab96
Daniel Svitan <dankosaurus9@gmail.com>
2021-08-02 17:29:42 +0200

    :bug: Fixes "no arguments -> seg fault bug"

--- src/main.c
+++ src/main.c
@@ -10,18 +10,20 @@
 int main(int argc, char* argv[]) {
     Result result;
 
-    if (argc < 1) {
+    if (argc <= 1) {
         return EXIT_SUCCESS;
     }
-    else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
-        printf("This is automatic response provided by help command.\n");
-        printf("Program arguments:\n");
-        printf("\t<encoder action (-e | -d)> - action you want the encoder to do (-e for encode, -d for decode).\n");
-        printf("\t<encoder string> - string you want to decode/encode\n");
+    else if (argc == 2) {
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+            printf("This is automatic response provided by help command.\n");
+            printf("Program arguments:\n");
+            printf("\t<encoder action (-e | -d)> - action you want the encoder to do (-e for encode, -d for decode).\n");
+            printf("\t<encoder string> - string you want to decode/encode\n");
 
-        return EXIT_SUCCESS;
+            return EXIT_SUCCESS;
+        }
     }
-    else if (argc >= 2) {
+    else if (argc >= 3) {
         if (strcmp(argv[1], "-d") == 0 || strcmp(argv[1], "--decode") == 0) {
             result = encode(argv[2], 1, 0);
         }
